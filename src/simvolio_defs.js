7d133ae2ad563910c83ff0a44d0e03f8d9b0f13d
CynepHy6 <CynepHy6@gmail.com>
2018-08-22 14:44:41 +0300

    removed ContractName

--- src/simvolio_defs.js
+++ src/simvolio_defs.js
@@ -437,12 +437,6 @@ const functionDefs = {
         insertText: 'ColumnCondition(',
         params: []
     },
-    ContractName: {
-        label: 'ContractName(code string) string',
-        documentation: '',
-        insertText: 'ContractName(',
-        params: []
-    },
     ValidateEditContractNewValue: {
         label: 'ValidateEditContractNewValue(newValue, oldValue string)',
         documentation: '',
